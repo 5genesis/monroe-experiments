29347b99df8273a09697dc0261ee77f144f87df4
Jonas Karlsson <jonas.karlsson@kau.se>
2021-02-08 14:42:53 +0100

    Deleted old container

--- browsertime/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-CONTAINER=${DIR##*/}
-
-docker pull $(awk '/FROM/{ print $2 }' Dockerfile)
-docker build --rm --no-cache -t ${CONTAINER} . && echo "Finished building ${CONTAINER}"
